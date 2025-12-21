3840748cff8dba59b08a5d3e82dcc0a432991323
github_user <68958481+Mrgaton@users.noreply.github.com>
2025-12-21 20:37:43 +0100

    Refactor submodule path and enhance window functionality in Program.cs

--- velocis-launcher/Program.cs
+++ velocis-launcher/Program.cs
@@ -2,6 +2,9 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Security.Principal;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 
 namespace VelocisLauncher
 {
@@ -38,28 +41,85 @@ namespace VelocisLauncher
 
             LibraryLoader. LoadNativeDlls(dllPath, ["Photino.Native.dll", "WebView2Loader.dll"]);
 
-            // Ensure the directory exists
             Directory.CreateDirectory(DataPath);
 
             Console.WriteLine("Launching Photino...");
 
             var window = new PhotinoWindow()
                 .SetTitle("Velocis App")
-                .SetSize(768, 512)
-                .SetDevToolsEnabled(Debug)
-                .SetFileSystemAccessEnabled(true)
-                .SetSmoothScrollingEnabled(true)
-                .SetUserAgent("VelocisLauncher/1.0")
+                .SetChromeless(true)
+                .SetUseOsDefaultSize(false)
+                .SetUseOsDefaultLocation(false)
+                .SetSize(768 * 2, 512 * 2)
                 .SetResizable(true)
+                .SetDevToolsEnabled(Debug)
+                .SetContextMenuEnabled(false)
+                .SetTemporaryFilesPath(DataPath)
+
                 .SetTemporaryFilesPath(DataPath)
                 .Center()
-                .Load(args.Length > 0 ? args[0] : "https://mold-willing-can-invitations.trycloudflare.com/");
+                
+                .Load(args.Length > 0 ? args[0] : "https://google.com/");
+
+            
+            window.SetChromeless(true);
 
             window.SetLogVerbosity(0);
 
             window.RegisterWebMessageReceivedHandler((_, e) =>
             {
                 Console.WriteLine($"Message from web content: {e}");
+
+                if (e[0] != '{' || e[e.Length - 1] != '}')
+                    return;
+
+                var obj = JsonNode.Parse(e);
+
+                switch ((string)obj["type"])
+                {
+                    case "close":
+                        window.Close();
+                        break;
+
+                    case "setTitle":
+                            var title = (string)obj["title"];
+                            window.SetTitle(title);
+                        break;
+
+                        case "setSize":
+                            var width = (int)obj["width"];
+                            var height = (int)obj["height"];
+                            window.SetSize(width, height);
+                        break;
+
+                        case "setPosition":
+                            var x = (int)obj["x"];
+                            var y = (int)obj["y"];
+                            window.MoveTo(x, y);
+                        break;
+
+                        case "minimize":
+                            window.SetMinimized(true);
+                        break;
+
+                        case "maximize":
+                            window.SetMaximized(true);
+                        break;
+                      
+                        case "navigate":
+                            var url = (string)obj["url"];
+                            window.Load(url);
+                        break; 
+
+                        case "log":
+                            var message = (string)obj["message"];
+                            Console.WriteLine($"Log from web content: {message}");
+                        break;
+
+                        case "alert":
+                            window.SendNotification((string)obj["title"], (string)obj["message"]);
+                        break;
+                }
 
                 window.SendWebMessage(e);
             });
