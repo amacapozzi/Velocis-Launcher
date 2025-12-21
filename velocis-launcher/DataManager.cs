24e4737da5a7ff2d496c2b5b6cdace4eb98ab684
github_user <68958481+Mrgaton@users.noreply.github.com>
2025-12-21 20:50:43 +0100

    Add my-lib as a submodule

--- velocis-launcher/DataManager.cs
+++ velocis-launcher/DataManager.cs
@@ -8,14 +8,21 @@ namespace VelocisLauncher
 {
     internal class DataManager
     {
-        public  class UserData
+        public class UserData
         {
             public string Username { get; set; }
             public string Token { get; set; }
-            public Dictionary<int,string> DonwloadedGames { get; set; }
+            public Dictionary<int,(string Path,string SourceUrl)> DonwloadedGames { get; set; } // GameID -> (Path, SourceUrl)
         }
 
         private static string UserDataPath = Path.Combine(Program.DataPath, "userdata.json");
+
+        public static UserData Data = GetData() ?? new UserData
+        {
+            Username = "",
+            Token = "",
+            DonwloadedGames = new Dictionary<int, (string Path, string SourceUrl)>()
+        };
 
         public static UserData? GetData() => !File.Exists(UserDataPath) ? null : JsonSerializer.Deserialize<UserData>(File.ReadAllText(UserDataPath));
         public static void SaveData(UserData ud) => File.WriteAllText(UserDataPath, JsonSerializer.Serialize(ud));
---
+++ velocis-launcher/GamesDownloader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VelocisLauncher
+{
+    internal class GamesDownloader
+    {
+        public static void DownloadGame(int gameId, string downloadUrl, string destinationPath)
+        {
+            Console.WriteLine($"Starting download for game ID {gameId} from {downloadUrl} to {destinationPath}...");
+
+
+        }
+    }
+}
